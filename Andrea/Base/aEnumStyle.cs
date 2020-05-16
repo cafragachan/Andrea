623321d3a4056c287fbd56ad8a288f6f10eb5e9c
cafragachan <cafragachan@yahoo.com>
2020-05-16 13:44:55 +0100

    Add project files.

---
+++ Andrea/Base/aEnumStyle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andrea.Base
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class aEnumStyle
+    {
+        public enum Design { modern, rustic};
+        public enum Structure { concrete, wood};
+    }
+}
